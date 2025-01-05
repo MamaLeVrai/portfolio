67d046bdfa8084e64e6e2ac60730599e567344cf
marius margueray <margueray.marius@gmail.com>
2025-01-05 14:46:21 +0100

    changement

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>Portfolio Louis Dumont-Derobert</title>
+    <title>Portfolio Marius MARGUERAY</title>
     <link href="assets/stylesheet.css" rel="stylesheet" />
   </head>
   <body>
